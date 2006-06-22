2adbcbb39405eb3ddb6ea41214e8ba200ed53042
hechee@blackhole.universe.org <>
2006-06-22 21:58:04 +0200

    Moved some unused definitions.

--- old-or-unused-stuff/alien-stuff.lisp
+++ old-or-unused-stuff/alien-stuff.lisp
@@ -1,3 +1,9 @@
+ (:integer host-not-found "HOST_NOT_FOUND")
+ (:integer no-data "NO_DATA")
+ (:integer no-address "NO_ADDRESS")
+ (:integer no-recovery "NO_RECOVERY")
+ (:integer try-again "TRY_AGAIN")
+
  (:structure hostent ("struct hostent"
                       (c-string-pointer name "char *" "h_name")
                       ((* c-string) aliases "char **" "h_aliases")
@@ -9,16 +15,14 @@
                        ((* (* (unsigned 8))) aliases "char **" "s_aliases")
                        (int port "int" "s_port")
                        (c-string-pointer proto "char *" "s_proto")))
+ (:structure netent ("struct netent"
+                     (c-string-pointer name "char *" "n_name")
+                     ((* c-string) aliases "char **" "n_aliases")
+                     (int type "int" "n_addrtype")
+                     (uint32-t net "int" "n_net")))
 
- (:integer host-not-found "HOST_NOT_FOUND")
- (:integer no-data "NO_DATA")
- (:integer no-address "NO_ADDRESS")
- (:integer no-recovery "NO_RECOVERY")
- (:integer try-again "TRY_AGAIN")
-
+;; Host lookup
 (define-alien-routine "endhostent" void)
-
-(define-alien-routine "endservent" void)
 
 (define-alien-routine "gethostbyaddr" (* (struct hostent))
   (addr (* t))
@@ -30,18 +34,36 @@
 
 (define-alien-routine "gethostent" (* (struct hostent)))
 
-(define-alien-routine "getservbyname" (* (struct servent))
-  (name c-string)
-  (proto c-string))
+(define-alien-routine "sethostent" void
+  (stayopen int))
+
+;; Service lookup
+(define-alien-routine "endservent" void)
 
 (define-alien-routine "getservbyport" (* (struct servent))
   (port int)
   (proto c-string))
 
+(define-alien-routine "getservbyname" (* (struct servent))
+  (name c-string)
+  (proto c-string))
+
 (define-alien-routine "getservent" (* (struct servent)))
 
-(define-alien-routine "sethostent" void
+(define-alien-routine "setservent" void
   (stayopen int))
 
-(define-alien-routine "setservent" void
+;; Net lookup
+(define-alien-routine "endnetent" void)
+
+(define-alien-routine "getnetbyaddr" (* (struct netent))
+  (net uint32-t)
+  (type int))
+
+(define-alien-routine "getnetbyname" (* (struct netent))
+  (name c-string))
+
+(define-alien-routine "getnetent" (* (struct netent)))
+
+(define-alien-routine "setnetent" void
   (stayopen int))
