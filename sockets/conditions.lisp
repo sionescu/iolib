228877e228fd58f9987a985512b9f63ad1bb3ac5
Stelian Ionescu <sionescu@common-lisp.net>
2007-02-03 21:22:40 +0100

    Switched licence to LLGPL.

--- sockets/conditions.lisp
+++ sockets/conditions.lisp
@@ -1,23 +1,23 @@
 ;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp -*-
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;   Copyright (C) 2006 by Stelian Ionescu                                 ;
-;                                                                         ;
-;   This program is free software; you can redistribute it and/or modify  ;
-;   it under the terms of the GNU General Public License as published by  ;
-;   the Free Software Foundation; either version 2 of the License, or     ;
-;   (at your option) any later version.                                   ;
-;                                                                         ;
-;   This program is distributed in the hope that it will be useful,       ;
-;   but WITHOUT ANY WARRANTY; without even the implied warranty of        ;
-;   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ;
-;   GNU General Public License for more details.                          ;
-;                                                                         ;
-;   You should have received a copy of the GNU General Public License     ;
-;   along with this program; if not, write to the                         ;
-;   Free Software Foundation, Inc.,                                       ;
-;   51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA              ;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;   Copyright (C) 2006, 2007 Stelian Ionescu
+;;
+;;   This code is free software; you can redistribute it and/or
+;;   modify it under the terms of the version 2.1 of
+;;   the GNU Lesser General Public License as published by
+;;   the Free Software Foundation, as clarified by the
+;;   preamble found here:
+;;       http://opensource.franz.com/preamble.html
+;;
+;;   This program is distributed in the hope that it will be useful,
+;;   but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;   GNU General Public License for more details.
+;;
+;;   You should have received a copy of the GNU Lesser General
+;;   Public License along with this library; if not, write to the
+;;   Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+;;   Boston, MA 02110-1301, USA
 
 (in-package :net.sockets)
 
