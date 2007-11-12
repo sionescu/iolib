9ec1f678af841c215fc55935945fb879a9abe6ab
Stelian Ionescu <sionescu@common-lisp.net>
2007-11-12 01:06:42 +0100

    Rehauled /etc/protocols parsing routines
    Signed-off-by: Stelian Ionescu <sionescu@common-lisp.net>

--- sockets/namedb/protocols.lisp
+++ sockets/namedb/protocols.lisp
@@ -23,6 +23,8 @@
 
 (in-package :net.sockets)
 
+(defvar *protocols-file* "/etc/protocols")
+
 (defclass protocol ()
   ((name :initarg :name :reader protocol-name
          :documentation "The protocol's primary name.")
@@ -34,29 +36,85 @@
 
 (defun make-protocol (name number &optional aliases)
   "Constructor for PROTOCOL objects."
-  (make-instance 'protocol :name name :number number :aliases aliases))
+  (let ((number (cond ((numberp number) number)
+                      ((string  number) (parse-integer number)))))
+    (make-instance 'protocol :name name :number number :aliases aliases)))
 
 (defmethod print-object ((protocol protocol) stream)
   (print-unreadable-object (protocol stream :type t :identity nil)
-    (with-slots (name aliases protonum) protocol
-      (format stream "Name: ~S Protocol number: ~A Aliases: ~{~S~^, ~}"
-              name protonum aliases))))
+    (with-slots (name aliases number) protocol
+      (format stream "Name: ~S Number: ~A Aliases: ~:[None~;~:*~{~S~^, ~}~]"
+              name number aliases))))
 
-(defun lookup-protocol-number (proto))
+(defun find-protocol-in-parsed-lines (tokens predicate)
+  (when (< (length tokens) 2) (error 'parse-error))
+  (destructuring-bind (name value &rest aliases) tokens
+    (let ((value (parse-integer value)))
+      (when (funcall predicate name value aliases)
+        (make-protocol name value aliases)))))
 
-(defun lookup-protocol-name (proto))
+(defun lookup-protocol-on-disk-by-name (file protocol)
+  (flet ((good-proto-p (name value aliases)
+           (declare (ignore value))
+           (or (string= protocol name)
+               (member protocol aliases :test #'string=))))
+    (iterate ((tokens (serialize-etc-file file)))
+      (ignore-some-conditions (parse-error)
+        (let ((proto (find-protocol-in-parsed-lines tokens #'good-proto-p)))
+          (when proto (return-from lookup-protocol-on-disk-by-name proto)))))))
+
+(defun lookup-protocol-on-disk-by-number (file protocol)
+  (flet ((good-proto-p (name value aliases)
+           (declare (ignore name aliases))
+           (= protocol value)))
+    (iterate ((tokens (serialize-etc-file file)))
+      (ignore-some-conditions (parse-error)
+        (let ((proto (find-protocol-in-parsed-lines tokens #'good-proto-p)))
+          (when proto (return-from lookup-protocol-on-disk-by-number proto)))))))
 
 (define-condition unknown-protocol ()
-  ((name :initarg :name :initform nil :reader protocol-name))
+  ((name :initarg :name :initform nil :reader unknown-protocol-name))
   (:report (lambda (condition stream)
-             (format stream "Unknown protocol: ~S" (protocol-name condition))))
+             (format stream "Unknown protocol: ~S" (unknown-protocol-name condition))))
   (:documentation "Condition raised when a network protocol is not found."))
+
+(defvar *protocols-cache-by-name*   (make-hash-table :test #'equal))
+(defvar *protocols-cache-by-number* (make-hash-table :test #'eql))
+
+(defun find-protocol (thing cache-fn disk-fn)
+  (or (funcall cache-fn thing)
+      (let ((protocol (funcall disk-fn *protocols-file* thing)))
+        (when protocol
+          (setf (gethash (protocol-name protocol) *protocols-cache-by-name*) protocol)
+          (dolist (alias (protocol-aliases protocol))
+            (setf (gethash alias *protocols-cache-by-name*) protocol))
+          (setf (gethash (protocol-number protocol) *protocols-cache-by-number*) protocol)
+          (values protocol)))))
+
+(defun lookup-protocol-by-name (proto)
+  (find-protocol proto
+                 #'(lambda (p) (gethash p *protocols-cache-by-name*))
+                 #'lookup-protocol-on-disk-by-name))
+
+(defun lookup-protocol-by-number (proto)
+  (find-protocol proto
+                 #'(lambda (p) (gethash p *protocols-cache-by-number*))
+                 #'lookup-protocol-on-disk-by-number))
 
 (defun lookup-protocol (protocol)
   "Lookup a protocol by name or number.  Signals an
 UNKNOWN-PROTOCOL error if no protocol is found."
   (let* ((parsed-number (parse-number-or-nil protocol))
          (proto (if parsed-number
-                    (lookup-protocol-number parsed-number)
-                    (lookup-protocol-name protocol))))
+                    (lookup-protocol-by-number parsed-number)
+                    (lookup-protocol-by-name protocol))))
     (or proto (error 'unknown-protocol :name protocol))))
+
+(defun purge-protocols-cache ()
+  (map 'nil #'clrhash (list *protocols-cache-by-name*
+                            *protocols-cache-by-number*)))
+
+(defvar *protocols-monitor*
+  (make-instance 'file-monitor
+                 :file *protocols-file*
+                 :update-fn 'purge-protocols-cache))
