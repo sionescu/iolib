f9cdd0b7220a7d95f10b1e6f2c8021af8665cc87
Stelian Ionescu <sionescu@common-lisp.net>
2008-01-06 20:44:53 +0100

    Use literal hash tables in defining DNS constants.
    Signed-off-by: Stelian Ionescu <sionescu@common-lisp.net>

--- sockets/dns/common.lisp
+++ sockets/dns/common.lisp
@@ -2,7 +2,7 @@
 ;;;
 ;;; common.lisp --- DNS client constants.
 ;;;
-;;; Copyright (C) 2006-2007, Stelian Ionescu  <sionescu@common-lisp.net>
+;;; Copyright (C) 2006-2008, Stelian Ionescu  <sionescu@common-lisp.net>
 ;;;
 ;;; This code is free software; you can redistribute it and/or
 ;;; modify it under the terms of the version 2.1 of
@@ -23,55 +23,65 @@
 
 (in-package :net.sockets)
 
+(defconstant +dns-max-datagram-size+ 4096)
+
 (defconstant +opcode-standard+ 0)
 
-(define-constant +query-type-map+
-    '((:a     .   1)
-      (:ns    .   2)
-      (:cname .   5)
-      (:soa   .   6)
-      (:wks   .  11)
-      (:ptr   .  12)
-      (:hinfo .  13)
-      (:mx    .  15)
-      (:txt   .  16)
-      (:aaaa  .  28)
-      (:any   . 255))
-  :test #'equal)
+;;; Query types
+
+(define-constant +query-type-to-value-map+
+    #h:eq{:a => 1 , :ns => 2 , :cname => 5 , :soa => 6 ,
+          :wks => 11 , :ptr => 12 , :hinfo => 13 , :mx => 15 ,
+          :txt => 16 , :aaaa => 28 , :any => 255}
+    :test #'equalp)
+
+(define-constant +query-value-to-type-map+
+    #h{1 => :a ,  2 => :ns , 5 => :cname , 6 => :soa ,
+       11 => :wks , 12 => :ptr , 13 => :hinfo , 15 => :mx ,
+       16 => :txt , 28 => :aaaa , 255 => :any}
+    :test #'equalp)
 
 (defun query-type-number (id)
-  (cdr (assoc id +query-type-map+)))
+  (gethash id +query-type-to-value-map+))
 
 (defun query-type-id (number)
-  (car (rassoc number +query-type-map+)))
+  (gethash number +query-value-to-type-map+))
 
 (defun dns-record-type-p (id)
   (query-type-number id))
 
-(define-constant +query-class-map
-    '((:in  .   1)
-      (:any . 255))
-  :test #'equal)
+;;; Query classes
+
+(define-constant +query-class-to-value-map+
+    #h:eq{:in => 1 , :any => 255}
+    :test #'equalp)
+
+(define-constant +query-value-to-class-map+
+    #h{1 => :in , 255 => :any}
+    :test #'equalp)
 
 (defun query-class-number (id)
-  (cdr (assoc id +query-class-map)))
+  (gethash id +query-class-to-value-map+))
 
 (defun query-class-id (number)
-  (car (rassoc number +query-class-map)))
+  (gethash number +query-value-to-class-map+))
 
-(define-constant +rcode-map+
-    '((:no-error        . 0)
-      (:format-error    . 1)
-      (:server-failure  . 2)
-      (:name-error      . 3)
-      (:not-implemented . 4)
-      (:refused         . 5))
-  :test #'equal)
+;;; Error codes
+
+(define-constant +rcode-to-value-map+
+    #h:eq{:no-error => 0 , :format-error => 1 ,
+          :server-failure => 2 , :name-error => 3 ,
+          :not-implemented => 4 , :refused => 5}
+    :test #'equalp)
+
+(define-constant +value-to-rcode-map+
+    #h{0 => :no-error , 1 => :format-error ,
+       2 => :server-failure , 3 => :name-error ,
+       4 => :not-implemented , 5 => :refused}
+    :test #'equalp)
 
 (defun rcode-number (id)
-  (cdr (assoc id +rcode-map+)))
+  (gethash id +rcode-to-value-map+))
 
 (defun rcode-id (number)
-  (car (rassoc number +rcode-map+)))
-
-(defconstant +dns-max-datagram-size+ 4096)
+  (gethash number +value-to-rcode-map+))
