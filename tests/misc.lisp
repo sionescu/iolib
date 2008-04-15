bf0ac5f86cfcb67aaf9aca852f2744bffcbf32c8
Stelian Ionescu <sionescu@common-lisp.net>
2008-04-15 16:30:33 +0200

    Fix literal hash tables tests.
    Signed-off-by: Stelian Ionescu <sionescu@common-lisp.net>

--- tests/misc.lisp
+++ tests/misc.lisp
@@ -28,38 +28,52 @@
 
 (in-suite* :iolib.misc :in :iolib)
 
+(defmacro with-literal-hash-table-syntax (&body body)
+  `(let ((*readtable* (copy-readtable)))
+     (unwind-protect
+          (progn
+            (enable-reader-macro 'literal-hash-table)
+            ,@body)
+       (disable-reader-macro 'literal-hash-table))))
+
 (test literal-hash-tables.creation.empty-hash-table-default-test
   (finishes
-    (read-from-string "#h()")))
+    (with-literal-hash-table-syntax
+      (read-from-string "#h()"))))
 
 (test (literal-hash-tables.type.empty-hash-table-default-test
        :depends-on literal-hash-tables.creation.empty-hash-table-default-test)
   (is-true
-   (let ((ht (read-from-string "#h()")))
+   (let ((ht (with-literal-hash-table-syntax
+               (read-from-string "#h()"))))
      (and (zerop (hash-table-count ht))
           (eq (hash-table-test ht) #-clisp 'eql
                                    #+clisp 'ext:fasthash-eql)))))
 
 (test literal-hash-tables.creation.empty-hash-table-test-eq
   (finishes
-    (read-from-string "#h:eq()")))
+    (with-literal-hash-table-syntax
+      (read-from-string "#h:eq()"))))
 
 (test (literal-hash-tables.type.empty-hash-table-test-eq
        :depends-on literal-hash-tables.creation.empty-hash-table-test-eq)
   (is-true
-   (let ((ht (read-from-string "#h:eq()")))
+   (let ((ht (with-literal-hash-table-syntax
+               (read-from-string "#h:eq()"))))
      (and (zerop (hash-table-count ht))
           (eq (hash-table-test ht) #-clisp 'eq
                                    #+clisp 'ext:fasthash-eq)))))
 
 (test literal-hash-tables.creation.one-element-hash-table-default-test
   (finishes
-    (read-from-string "#h((1 . 3))")))
+    (with-literal-hash-table-syntax
+      (read-from-string "#h((1 . 3))"))))
 
 (test (literal-hash-tables.type.one-element-hash-table-default-test
        :depends-on literal-hash-tables.creation.one-element-hash-table-default-test)
   (is-true
-   (let ((ht (read-from-string "#h((1 . 3))")))
+   (let ((ht (with-literal-hash-table-syntax
+               (read-from-string "#h((1 . 3))"))))
      (and (= 1 (hash-table-count ht))
           (eq (hash-table-test ht) #-clisp 'eql
                                    #+clisp 'ext:fasthash-eql)
@@ -67,12 +81,14 @@
 
 (test literal-hash-tables.creation.one-element-hash-table-test-eq
   (finishes
-    (read-from-string "#h:eq((:a . :test))")))
+    (with-literal-hash-table-syntax
+      (read-from-string "#h:eq((:a . :test))"))))
 
 (test (literal-hash-tables.type.one-element-hash-table-test-eq
        :depends-on literal-hash-tables.creation.one-element-hash-table-test-eq)
   (is-true
-   (let ((ht (read-from-string "#h:eq((:a . :test))")))
+   (let ((ht (with-literal-hash-table-syntax
+               (read-from-string "#h:eq((:a . :test))"))))
      (and (= 1 (hash-table-count ht))
           (eq (hash-table-test ht) #-clisp 'eq
                                    #+clisp 'ext:fasthash-eq)
@@ -80,8 +96,10 @@
 
 (test literal-hash-tables.error.wrong-test
   (signals error
-    (read-from-string "#h:nologo((2 . 3))")))
+    (with-literal-hash-table-syntax
+      (read-from-string "#h:nologo((2 . 3))"))))
 
 (test literal-hash-tables.error.wrong-element
   (signals reader-error
-    (read-from-string "#h(2)")))
+    (with-literal-hash-table-syntax
+      (read-from-string "#h(2)"))))
