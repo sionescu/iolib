1fe7bb52fff4b33db76d1c1dbe7937263ae6bdfb
Stelian Ionescu <sionescu@common-lisp.net>
2008-08-20 05:02:11 +0200

    Remove useless comments from SPLIT-SEQUENCE.
    Signed-off-by: Stelian Ionescu <sionescu@common-lisp.net>

--- base/split-sequence.lisp
+++ base/split-sequence.lisp
@@ -5,30 +5,6 @@
 ;;; This code was based on Arthur Lemmens' in
 ;;; <URL:http://groups.google.com/groups?as_umsgid=39F36F1A.B8F19D20%40simplex.nl>;
 ;;;
-;;; changes include:
-;;;
-;;; * altering the behaviour of the :from-end keyword argument to
-;;; return the subsequences in original order, for consistency with
-;;; CL:REMOVE, CL:SUBSTITUTE et al. (:from-end being non-NIL only
-;;; affects the answer if :count is less than the number of
-;;; subsequences, by analogy with the above-referenced functions).
-;;;
-;;; * changing the :maximum keyword argument to :count, by analogy
-;;; with CL:REMOVE, CL:SUBSTITUTE, and so on.
-;;;
-;;; * naming the function SPLIT-SEQUENCE rather than PARTITION rather
-;;; than SPLIT.
-;;;
-;;; * adding SPLIT-SEQUENCE-IF and SPLIT-SEQUENCE-IF-NOT.
-;;;
-;;; * The second return value is now an index rather than a copy of a
-;;; portion of the sequence; this index is the `right' one to feed to
-;;; CL:SUBSEQ for continued processing.
-
-;;; There's a certain amount of code duplication here, which is kept
-;;; to illustrate the relationship between the SPLIT-SEQUENCE
-;;; functions and the CL:POSITION functions.
-
 ;;; Examples:
 ;;;
 ;;; * (split-sequence #\; "a;;b;c")
