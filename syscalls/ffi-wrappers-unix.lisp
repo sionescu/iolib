9f3800e281af4bd7e7b414c80c0b87b85fd5776c
Stelian Ionescu <sionescu@common-lisp.net>
2008-08-23 20:59:02 +0200

    Small fixes to syscall wrappers.
    Signed-off-by: Stelian Ionescu <sionescu@common-lisp.net>

--- syscalls/ffi-wrappers-unix.lisp
+++ syscalls/ffi-wrappers-unix.lisp
@@ -24,72 +24,74 @@
 
 (defwrapper ("lseek" %sys-lseek)
     ("off_t" (return-wrapper off-t :error-generator signal-posix-error))
-  (fildes ("int" :int))
-  (offset ("off_t" off-t))
+  (fildes :int)
+  (offset off-t)
   (whence :int))
 
 (defwrapper ("truncate" %sys-truncate)
     ("int" (return-wrapper :int :error-generator signal-posix-error/restart))
   (path ("const char*" filename-designator))
-  (length ("off_t" off-t)))
+  (length off-t))
 
 (defwrapper ("ftruncate" %sys-ftruncate)
     ("int" (return-wrapper :int :error-generator signal-posix-error/restart))
-  (fd ("int" :int))
-  (length ("off_t" off-t)))
+  (fd     :int)
+  (length off-t))
 
 (defwrapper ("mmap" %sys-mmap)
     ("void*" (return-wrapper :pointer :error-generator signal-posix-error))
-  (start :pointer)
-  (length ("size_t" size-t))
-  (prot :int)
-  (flags :int)
-  (fd ("int" :int))
-  (offset ("off_t" off-t)))
+  (start  :pointer)
+  (length size-t)
+  (prot   :int)
+  (flags  :int)
+  (fd     :int)
+  (offset off-t))
 
 (defwrapper ("stat" %%sys-stat)
     ("int" (return-wrapper :int :error-generator signal-posix-error))
   (file-name ("const char*" filename-designator))
-  (buf ("struct stat*" :pointer)))
+  (buf       ("struct stat*" :pointer)))
 
 (defwrapper ("fstat" %%sys-fstat)
     ("int" (return-wrapper :int :error-generator signal-posix-error))
-  (filedes ("int" :int))
-  (buf ("struct stat*" :pointer)))
+  (filedes :int)
+  (buf     ("struct stat*" :pointer)))
 
 (defwrapper ("lstat" %%sys-lstat)
     ("int" (return-wrapper :int :error-generator signal-posix-error))
   (file-name ("const char*" filename-designator))
-  (buf ("struct stat*" :pointer)))
+  (buf       ("struct stat*" :pointer)))
 
 (defwrapper ("pread" %sys-pread)
     ("ssize_t" (return-wrapper ssize-t :error-generator signal-posix-error/restart))
-  (fd ("int" (return-wrapper :int :error-generator signal-posix-error)))
-  (buf :pointer)
-  (count ("size_t" size-t))
-  (offset ("off_t" off-t)))
+  (fd     :int)
+  (buf    :pointer)
+  (count  size-t)
+  (offset off-t))
 
 (defwrapper ("pwrite" %sys-pwrite)
     ("ssize_t" (return-wrapper ssize-t :error-generator signal-posix-error/restart))
-  (fd ("int" :int))
-  (buf :pointer)
-  (count ("size_t" size-t))
-  (offset ("off_t" off-t)))
+  (fd     :int)
+  (buf    :pointer)
+  (count  size-t)
+  (offset off-t))
 
 
 ;;;-----------------------------------------------------------------------------
 ;;; ERRNO-related functions
 ;;;-----------------------------------------------------------------------------
 
-(defwrapper* "get_errno" :int ()
+(defwrapper* "get_errno" :int
+  ()
   "return errno;")
 
-(defwrapper* "set_errno" :int ((value :int))
+(defwrapper* "set_errno" :int
+  ((value :int))
   "errno = value;"
   "return errno;")
 
 (defwrapper ("strerror_r" %sys-strerror-r)
     ("int" (return-wrapper :int :error-generator signal-posix-error))
   (errnum :int)
-  (buf :string)
+  (buf    :string)
   (buflen size-t))
