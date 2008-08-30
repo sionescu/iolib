ecbea6390387add7b98f9ffdd94a4c7186c1c854
Stelian Ionescu <sionescu@common-lisp.net>
2008-08-30 10:45:29 +0200

    Add INLINE declamations for foreign wrappers.

--- syscalls/ffi-wrappers-unix.lisp
+++ syscalls/ffi-wrappers-unix.lisp
@@ -22,22 +22,26 @@
 
 ;;; FIXME: this is only necessary on Linux right?
 
+(declaim (inline %sys-lseek))
 (defwrapper ("lseek" %sys-lseek)
     ("off_t" (return-wrapper off-t :error-generator signal-posix-error))
   (fildes :int)
   (offset off-t)
   (whence :int))
 
+(declaim (inline %sys-truncate))
 (defwrapper ("truncate" %sys-truncate)
     ("int" (return-wrapper :int :error-generator signal-posix-error/restart))
   (path ("const char*" filename-designator))
   (length off-t))
 
+(declaim (inline %sys-ftruncate))
 (defwrapper ("ftruncate" %sys-ftruncate)
     ("int" (return-wrapper :int :error-generator signal-posix-error/restart))
   (fd     :int)
   (length off-t))
 
+(declaim (inline %sys-mmap))
 (defwrapper ("mmap" %sys-mmap)
     ("void*" (return-wrapper :pointer :error-generator signal-posix-error))
   (start  :pointer)
@@ -47,21 +51,25 @@
   (fd     :int)
   (offset off-t))
 
+(declaim (inline %%sys-stat))
 (defwrapper ("stat" %%sys-stat)
     ("int" (return-wrapper :int :error-generator signal-posix-error))
   (file-name ("const char*" filename-designator))
   (buf       ("struct stat*" :pointer)))
 
+(declaim (inline %%sys-fstat))
 (defwrapper ("fstat" %%sys-fstat)
     ("int" (return-wrapper :int :error-generator signal-posix-error))
   (filedes :int)
   (buf     ("struct stat*" :pointer)))
 
+(declaim (inline %%sys-lstat))
 (defwrapper ("lstat" %%sys-lstat)
     ("int" (return-wrapper :int :error-generator signal-posix-error))
   (file-name ("const char*" filename-designator))
   (buf       ("struct stat*" :pointer)))
 
+(declaim (inline %sys-pread))
 (defwrapper ("pread" %sys-pread)
     ("ssize_t" (return-wrapper ssize-t :error-generator signal-posix-error/restart))
   (fd     :int)
@@ -69,6 +77,7 @@
   (count  size-t)
   (offset off-t))
 
+(declaim (inline %sys-pwrite))
 (defwrapper ("pwrite" %sys-pwrite)
     ("ssize_t" (return-wrapper ssize-t :error-generator signal-posix-error/restart))
   (fd     :int)
@@ -81,15 +90,18 @@
 ;;; ERRNO-related functions
 ;;;-----------------------------------------------------------------------------
 
+(declaim (inline %sys-errno))
 (defwrapper* ("iolib_get_errno" %sys-errno) :int
   ()
   "return errno;")
 
+(declaim (inline %%sys-set-errno))
 (defwrapper* ("iolib_set_errno" %%sys-set-errno) :int
   ((value :int))
   "errno = value;"
   "return errno;")
 
+(declaim (inline %sys-strerror-r))
 (defwrapper ("strerror_r" %sys-strerror-r)
     ("int" (return-wrapper :int :error-generator signal-posix-error))
   (errnum :int)
