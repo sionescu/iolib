86f5398880b4bd3f8605bc0c5c15c7c39f24e139
Stelian Ionescu <sionescu@common-lisp.net>
2009-01-07 01:54:35 +0100

    Add ZSTREAM-{READ,WRITE}-VECTOR.

--- io.streams/zeta/stream.lisp
+++ io.streams/zeta/stream.lisp
@@ -23,9 +23,68 @@
 ;;; Generic Functions
 ;;;-------------------------------------------------------------------------
 
+(defgeneric (setf external-format-of) (external-format stream))
+
+(defgeneric zstream-read-vector (stream sequence &key start end))
+
+(defgeneric zstream-write-vector (stream sequence &key start end))
+
+
+;;;-------------------------------------------------------------------------
+;;; Accessors
+;;;-------------------------------------------------------------------------
+
+(defmethod (setf external-format-of)
+    (external-format (stream zeta-stream))
+  (setf (slot-value stream 'external-format)
+        (babel:ensure-external-format external-format)))
 
 
 ;;;-------------------------------------------------------------------------
 ;;; Constructors
 ;;;-------------------------------------------------------------------------
 
+(defmethod shared-initialize :after ((stream zeta-stream) slot-names
+                                     &key (external-format :default))
+  (declare (ignore slot-names))
+  (setf (external-format-of stream) external-format))
+
+
+;;;-------------------------------------------------------------------------
+;;; READ-VECTOR
+;;;-------------------------------------------------------------------------
+
+(defmethod zstream-read-vector ((stream zeta-stream) (vector vector)
+                                &key (start 0) end timeout)
+  (check-type vector (or ub8-simple-vector ub8-vector))
+  (check-bounds vector start end)
+  (when (= start end) (return* 0))
+  (device-read stream vector :start start :end end :timeout timeout))
+
+(defmethod zstream-read-vector ((stream zeta-stream) (vector string)
+                                &key (start 0) end timeout)
+  (check-type vector (or ub8-simple-vector ub8-vector))
+  (check-bounds vector start end)
+  (when (= start end) (return* 0))
+  ;; TODO: write it
+  )
+
+
+;;;-------------------------------------------------------------------------
+;;; WRITE-VECTOR
+;;;-------------------------------------------------------------------------
+
+(defmethod zstream-write-vector ((stream zeta-stream) (vector vector)
+                                 &key (start 0) end timeout)
+  (check-type vector (or ub8-simple-vector ub8-vector))
+  (check-bounds vector start end)
+  (when (= start end) (return* 0))
+  (device-write stream vector :start start :end end :timeout timeout))
+
+(defmethod zstream-write-vector ((stream zeta-stream) (vector string)
+                                 &key (start 0) end timeout)
+  (check-type vector (or ub8-simple-vector ub8-vector))
+  (check-bounds vector start end)
+  (when (= start end) (return* 0))
+  ;; TODO: write it
+  )
