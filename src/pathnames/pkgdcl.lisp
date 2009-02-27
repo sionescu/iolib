cb722bb7a493c14de932b021b6013cc60a269d11
Stelian Ionescu <sionescu@common-lisp.net>
2009-02-27 11:10:27 +0100

    Export FILE-PATH methods and classes.

--- src/pathnames/pkgdcl.lisp
+++ src/pathnames/pkgdcl.lisp
@@ -9,4 +9,31 @@
   (:nicknames #:ipath)
   (:use :iolib.base)
   (:export
-   ))
+   ;; Classes
+   #:file-path
+   #:unix-path
+
+   ;; Accessors
+   #:file-path-host
+   #:file-path-device
+   #:file-path-directory
+   #:file-path-name
+   #:file-path-directory-delimiter
+   #:file-path-alternative-delimiter
+   #:file-path-execution-path-delimiter
+   #:file-path-namestring
+
+   ;; Constructors
+   #:make-file-path
+   #:parse-file-path
+   #:file-path
+
+   ;; Operations
+   #:merge-file-paths
+   #:enough-file-path
+   #:concatenate-paths
+
+   ;; Predicates
+   #:file-path-p
+   #:file-path-absolute-p
+   #:file-path-relative-p))
