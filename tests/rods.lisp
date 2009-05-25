fb47fd436c1c2ddaab34236c23395e4da25710cf
Stelian Ionescu <sionescu@common-lisp.net>
2009-05-25 14:37:39 +0200

    Use CL:EQL instead of CL:= in the tests.

--- tests/rods.lisp
+++ tests/rods.lisp
@@ -12,10 +12,10 @@
   (is-true (typep (make-rod 1) '(rod 1))))
 
 (test make-rod.2
-  (is-true (= 0 (aref (make-rod 1) 0))))
+  (is-true (eql 0 (aref (make-rod 1) 0))))
 
 (test make-rod.3
-  (is-true (= 40 (aref (make-rod 1 :initial-element 40) 0))))
+  (is-true (eql 40 (aref (make-rod 1 :initial-element 40) 0))))
 
 (test make-rod.error.1
   (signals type-error
