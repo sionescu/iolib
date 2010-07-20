83cb7bea145eb1bc8af7825c7fface5f1145c2e4
Stelian Ionescu <sionescu@cddr.org>
2010-07-20 21:50:19 +0800

    Cosmetic changes

--- src/base/gray-stream-mixin.lisp
+++ src/base/gray-stream-mixin.lisp
@@ -9,10 +9,12 @@
 
 (defgeneric stream-read-sequence
     (stream sequence start end &key &allow-other-keys))
+
 (defgeneric stream-write-sequence
     (stream sequence start end &key &allow-other-keys))
 
 (defgeneric stream-file-position (stream))
+
 (defgeneric (setf stream-file-position) (newval stream))
 
 (defmethod stream-write-string
@@ -37,7 +39,8 @@
   (defmethod excl:stream-read-sequence
       ((s trivial-gray-stream-mixin) seq &optional start end)
     (stream-read-sequence s seq (or start 0) (or end (length seq))))
-  (defmethod stream:stream-write-sequence
+
+  (defmethod excl:stream-write-sequence
       ((s trivial-gray-stream-mixin) seq &optional start end)
     (stream-write-sequence s seq (or start 0) (or end (length seq)))))
 
@@ -46,6 +49,7 @@
   (defmethod ext:stream-read-sequence
       ((s trivial-gray-stream-mixin) seq &optional start end)
     (stream-read-sequence s seq (or start 0) (or end (length seq))))
+
   (defmethod ext:stream-write-sequence
       ((s trivial-gray-stream-mixin) seq &optional start end)
     (stream-write-sequence s seq (or start 0) (or end (length seq)))))
@@ -55,12 +59,14 @@
   (defmethod stream:stream-read-sequence
       ((s trivial-gray-stream-mixin) seq start end)
     (stream-read-sequence s seq start end))
+
   (defmethod stream:stream-write-sequence
       ((s trivial-gray-stream-mixin) seq start end)
     (stream-write-sequence s seq start end))
 
   (defmethod stream:stream-file-position ((stream trivial-gray-stream-mixin))
     (stream-file-position stream))
+
   (defmethod (setf stream:stream-file-position)
       (newval (stream trivial-gray-stream-mixin))
     (setf (stream-file-position stream) newval)))
@@ -70,6 +76,7 @@
   (defmethod ccl:stream-read-vector
       ((s trivial-gray-stream-mixin) seq start end)
     (stream-read-sequence s seq start end))
+
   (defmethod ccl:stream-write-vector
       ((s trivial-gray-stream-mixin) seq start end)
     (stream-write-sequence s seq start end)))
@@ -102,9 +109,11 @@
   (defmethod sb-gray:stream-read-sequence
       ((s trivial-gray-stream-mixin) seq &optional start end)
     (stream-read-sequence s seq (or start 0) (or end (length seq))))
+
   (defmethod sb-gray:stream-write-sequence
       ((s trivial-gray-stream-mixin) seq &optional start end)
     (stream-write-sequence s seq (or start 0) (or end (length seq))))
+
   ;; SBCL extension:
   (defmethod sb-gray:stream-line-length ((stream trivial-gray-stream-mixin))
     80))
@@ -113,9 +122,11 @@
 (progn
   (when (fboundp 'gray::redefine-cl-functions)
     (gray::redefine-cl-functions))
+
   (defmethod gray:stream-read-sequence
       ((s trivial-gray-stream-mixin) seq &optional start end)
     (stream-read-sequence s seq (or start 0) (or end (length seq))))
+
   (defmethod gray:stream-write-sequence
       ((s trivial-gray-stream-mixin) seq &optional start end)
     (stream-write-sequence s seq (or start 0) (or end (length seq)))))
