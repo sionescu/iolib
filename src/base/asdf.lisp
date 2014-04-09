c7fe3ede1c029668dcf4c6e5752107a7e7552473
Stelian Ionescu <sionescu@cddr.org>
2014-04-09 11:49:25 +0200

    Bind CFFI:*FOREIGN-LIBRARY-DIRECTORIES* to NIL around compilation

--- src/base/asdf.lisp
+++ src/base/asdf.lisp
@@ -12,11 +12,16 @@
 (in-package :iolib.asdf)
 
 (defun compile-wrapper (continuation)
-  (let (;; Compilation fails because of CFFI types that
-        ;; can't be printed readably, so bind to NIL
-        (*print-readably* nil)
-        (*readtable* (copy-readtable))
+  (let ((*readtable* (copy-readtable))
         (uiop:*uninteresting-compiler-conditions*
           (append '(#+sbcl sb-int:package-at-variance)
                   uiop:*uninteresting-compiler-conditions*)))
-    (funcall continuation)))
+    (multiple-value-bind (symbols values)
+        (if (find-package :cffi)
+            (values (find-symbol #.(string '#:*foreign-library-directories*) :cffi) '(nil))
+            (values nil nil))
+      (with-standard-io-syntax
+        (let (;; Compilation fails because of CFFI types that
+              ;; can't be printed readably, so bind to NIL
+              (*print-readably* nil))
+          (funcall continuation))))))
