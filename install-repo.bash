454490058e663f80acd5772ac95b3dda55d90baa
Stelian Ionescu <sionescu@cddr.org>
2015-07-21 02:47:04 +0200

    Add TravisCI config file

---
+++ install-repo.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cat >> /etc/apt/sources.list <<EOF
+deb http://download.opensuse.org/repositories/home:/sionescu/Ubuntu_Precise/ ./
+EOF
+
+cat >> /etc/apt/preferences <<EOF
+
+Package: libfixposix*
+Pin: origin download.opensuse.org
+Pin-Priority: 1001
+EOF
+
+apt-get update
+
+apt-get -y --force-yes install libfixposix2 libfixposix-dev
