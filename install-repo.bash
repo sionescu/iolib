d7120285cd5a1e21e6ed06571962a1afe13daa55
Stelian Ionescu <sionescu@cddr.org>
2017-12-15 19:17:55 -0500

    Fix URL of Trusty repository

--- install-repo.bash
+++ install-repo.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cat >> /etc/apt/sources.list <<EOF
-deb http://download.opensuse.org/repositories/home:/sionescu/Ubuntu_Trusty/ ./
+deb http://download.opensuse.org/repositories/home:/sionescu/Ubuntu_14.04/ ./
 EOF
 
 cat >> /etc/apt/preferences <<EOF
