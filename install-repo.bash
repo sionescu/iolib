1c7861341f9f86004bf72b4e9f0b390b04627b04
Stelian Ionescu <sionescu@cddr.org>
2017-12-15 23:12:31 -0500

    Add error handling to install-repo.sh

--- install-repo.bash
+++ install-repo.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+set -o pipefail  # trace ERR through pipes
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o xtrace    # print commands as they are executed
+
 cat >> /etc/apt/sources.list <<EOF
 deb http://download.opensuse.org/repositories/home:/sionescu/Ubuntu_14.04/ ./
 EOF
