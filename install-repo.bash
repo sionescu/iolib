14da9b1b78c6492fc0b5af0f50765be2af395ac5
Stelian Ionescu <sionescu@cddr.org>
2022-11-10 14:07:03 -0500

    Use latest libfixposix on CI

--- install-repo.bash
+++ install-repo.bash
@@ -11,8 +11,9 @@ case "$1" in
     allegro | *32 ) ARCH=i386 ;;
 esac
 
+KEYFILE=/etc/apt/keyrings/obs-sionescu.key
 cat > /etc/apt/sources.list.d/libfixposix.list <<EOF
-deb http://download.opensuse.org/repositories/home:/sionescu/Debian_Old/ ./
+deb [signed-by=${KEYFILE}] http://download.opensuse.org/repositories/home:/sionescu/Debian/ ./
 EOF
 
 cat > /etc/apt/preferences.d/libfixposix <<EOF
@@ -21,10 +22,10 @@ Pin: origin download.opensuse.org
 Pin-Priority: 1001
 EOF
 
-curl https://download.opensuse.org/repositories/home:/sionescu/Debian_Old/Release.key | apt-key add -
+curl https://download.opensuse.org/repositories/home:/sionescu/Debian/Release.key | sudo tee "${KEYFILE}"
 
 apt-get update -qq
 
-apt-get -y install libfixposix3:${ARCH} libfixposix-dev:${ARCH}
+apt-get -y install libfixposix4:${ARCH} libfixposix-dev:${ARCH}
 
-dpkg -L libfixposix3 libfixposix-dev
+dpkg -L libfixposix4 libfixposix-dev
